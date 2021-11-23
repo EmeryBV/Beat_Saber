95d496ecceb34d0f3adf1387feb3f6fa5b1d81bf
«EmeryBV <>
2021-11-23 23:33:50 +0100

    update

--- Beat Saber/Assets/Scripts/Score/effectMultiplicator.cs
+++ Beat Saber/Assets/Scripts/Score/effectMultiplicator.cs
@@ -15,27 +15,32 @@ public class effectMultiplicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameData.multiplicatorChange)
-        RunCo();
+        if(gameData.multiplicatorChange){
+ 		RunCo();
+       
+        }
+       
     }
 
     private IEnumerator Pulse()
     {
+        multiplicator.color= new Color(1f, 1f - gameData.multiplierCurrent*0.01f, 1f-gameData.multiplierCurrent*0.01f, 1.0f); 
         for ( float i = 1.0f; i <=1.4f; i+=0.05f)
         {
             multiplicator.rectTransform.localScale = new Vector3(i, i, i);
             yield return new WaitForEndOfFrame();
         }
         int multiplicatorNumber = gameData.multiplierCurrent;
-        multiplicator.text= multiplicatorNumber.ToString();
+        multiplicator.text= "x" + multiplicatorNumber.ToString();
         for ( float i = 1.4f; i <=1.0f; i-=0.05f)
         {
             multiplicator.rectTransform.localScale = new Vector3(i, i, i);
-            // Debug.Log("here" +i );
             yield return new WaitForEndOfFrame();
         }
         multiplicator.rectTransform.localScale = new Vector3(1f, 1f, 1f);
         gameData.multiplicatorChange = false;
+        gameData.multiplierCurrent += 1;
+        yield return new WaitForSecondsRealtime(1);
     }
 
     public void RunCo()
